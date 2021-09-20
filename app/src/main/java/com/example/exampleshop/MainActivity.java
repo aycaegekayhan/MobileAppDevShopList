34d7f0604d8483135904df1f0eb0e6bca6dfa4a1
aycaegekayhan <akayhan1@binghamton.edu>
2021-09-20 10:25:16 -0400

    remove operation linked to onclick

--- app/src/main/java/com/example/exampleshop/MainActivity.java
+++ app/src/main/java/com/example/exampleshop/MainActivity.java
@@ -42,19 +42,21 @@ public class MainActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String name = items.get(i);
-                makeToast(name);
+                //String name = items.get(i);
+                //makeToast(name);
+                makeToast("Removed " + items.get(i));
+                removeItem(i);
             }
         });
 
-        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
-                makeToast("Removed " + items.get(i));
-                removeItem(i);
-                return false;
-            }
-        });
+        //listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            //@Override
+            //public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //makeToast("Removed " + items.get(i));
+                //removeItem(i);
+                //return false;
+            //}
+        //});
 
         adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, items);
         listView.setAdapter(adapter);
